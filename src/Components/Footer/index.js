4c1059dce103decdda2c3a38c0932a6e82ded743
rohitazad <ram9953878727@gmail.com>
2022-09-01 10:41:08 +0530

    footer code crreated

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,12 +1,47 @@
-import React from "react";
-import './Footer.style.scss';
 
-export default function FooterIndex(){
-    return(
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import style from './footer.module.scss';
+
+const FooterIndex = ()=>{
+    
+    return (
         <>
-        <footer className="footer">
-           My Footer
-        </footer>
+            <footer style={{ backgroundImage: 'url("img/footerbg.png")'}} className={style.customeFooter}>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <div className={style.copyRightText}>
+                                Copyrights © 2022 All Rights Reserved by R.A.M
+                            </div>
+                            <ul>
+                                <li>
+                                    <span>Follow us :- </span>
+                                </li>
+                                <li>
+                                    <a style={{backgroundImage:'url(/img/social_icons.jpg)'}} href="https://www.facebook.com/IamRohitAzad" target="_blank" rel="noreferrer" className={style.fb}>facebook</a>
+                                </li>
+                                <li>
+                                    <a style={{backgroundImage:'url(/img/social_icons.jpg)'}} href="https://twitter.com/rohitazad"   target="_blank" rel="noreferrer" className={style.twit}>twitter</a>
+                                </li>
+                                <li>
+                                    <a style={{backgroundImage:'url(/img/social_icons.jpg)'}} href="https://www.linkedin.com/in/rohitazad/"  target="_blank" rel="noreferrer" className={style.linkden}>linkdeen</a>
+                                </li>
+                                <li>
+                                    <a style={{backgroundImage:'url(/img/social_icons.jpg)'}} href="https://www.instagram.com/rohit_azad_malik/" rel="noreferrer"  target="_blank" className={style.insta}>instagram</a>
+                                </li>
+                                <li>
+                                    <a style={{backgroundImage:'url(/img/social_icons.jpg)'}} href="https://wa.me/+919953878727?text=Hi  Rohit " rel="noreferrer"  target="_blank" className={style.wat}>watsup</a>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
         </>
     )
 }
+
+export default FooterIndex;
